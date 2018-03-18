3c63893a6e46f47a3c0618e958d490ee7120132e
Emi Clark <emiko.clark@gmail.com>
2018-03-18 17:02:22 -0400

    return jsonDictionary

--- PokemonAPI/PokemonAPI/APIClient.swift
+++ PokemonAPI/PokemonAPI/APIClient.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class APIClient {
+    
+    class func getPokemonData(completion:@escaping([String: Any])->()) {
+        var jsonDictionary = [String:Any]()
+        
+        let urlString = "http://pokeapi.co/api/v2/pokemon/"
+        let urlconverted = URL(string: urlString)
+        guard let url = urlconverted else { print("failed"); return }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let dataUnwrapped = data else { print("data failed"); return }
+            
+            let jsonDict = try? JSONSerialization.jsonObject(with: dataUnwrapped, options: []) as! [String:Any]
+            guard let json = jsonDict else { print("json conversion failed"); return }
+            
+            jsonDictionary = json
+            completion(jsonDictionary)
+        }
+    }
+    
+}
