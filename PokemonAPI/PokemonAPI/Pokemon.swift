952e5657c020369a37db42104d2d5c27bf6ceb95
Emi Clark <emiko.clark@gmail.com>
2018-03-18 17:26:27 -0400

    created 2 model files

---
+++ PokemonAPI/PokemonAPI/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokemonAPI
+//
+//  Created by Emiko Clark on 3/18/18.
+//  Copyright © 2018 Emiko Clark. All rights reserved.
+//
+
+import Foundation
---
+++ PokemonAPI/PokemonAPI/PokemonDataStore.swift
@@ -0,0 +1,10 @@
+//
+//  PokemonDataStore.swift
+//  PokemonAPI
+//
+//  Created by Emiko Clark on 3/18/18.
+//  Copyright © 2018 Emiko Clark. All rights reserved.
+//
+
+import Foundation
+
