f486c3550d008b5b4492b8b990cd00f48dd1cf49
Emi Clark <emiko.clark@gmail.com>
2018-03-18 17:34:27 -0400

    created swift object

--- PokemonAPI/PokemonAPI/Pokemon.swift
+++ PokemonAPI/PokemonAPI/Pokemon.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class Pokemon {
+    
+    var url : String?
+    var name: String?
+    
+    init(jsonDictionary: [String:Any]) {
+        guard let url = jsonDictionary["url"] as? String,
+            let name = jsonDictionary["name"] as? String
+            else { print("url initialization failed"); return}
+                
+        self.name = name
+        self.url = url
+    }
+}
+
