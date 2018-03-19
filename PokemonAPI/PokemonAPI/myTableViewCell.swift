8729b88a7c3d289233ee57517fb763431c0b9a16
Emi Clark <emiko.clark@gmail.com>
2018-03-19 17:55:06 -0400

    cleanup other files

--- PokemonAPI/PokemonAPI/myTableViewCell.swift
+++ PokemonAPI/PokemonAPI/myTableViewCell.swift
@@ -13,15 +13,11 @@ class myTableViewCell: UITableViewCell {
     @IBOutlet weak var url: UILabel!
     @IBOutlet weak var name: UILabel!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
